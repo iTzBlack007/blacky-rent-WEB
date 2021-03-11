1e29489e940966a640741598f6b8667eaf1896ea
blacky <56867482+iTzBlack007@users.noreply.github.com>
2021-03-11 13:10:24 +0530

    Added a new feature : Auto Pinger 🔥

--- run.sh
+++ run.sh
@@ -1,8 +1,46 @@
 #!/bin/bash
 
-version=$(curl -s https://api.github.com/repos/boypt/simple-torrent/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
+get_latest_version() {
+	local uri
+	local version
+	uri="https://api.github.com/repos/boypt/simple-torrent/releases/latest"
+	version=$(
+		curl -s "$uri" |
+			grep -oP '"tag_name": "\K(.*)(?=")'
+	)
+	echo "$version"
+}
 
-wget -q https://github.com/boypt/simple-torrent/releases/download/"$version"/cloud-torrent_linux_amd64.gz
-gzip -d cloud-torrent_linux_amd64.gz
-chmod +x cloud-torrent_linux_amd64
-./cloud-torrent_linux_amd64
+get_url() {
+	local version
+	version=$(get_latest_version)
+	echo "https://github.com/boypt/simple-torrent/releases/download/"$version"/cloud-torrent_linux_amd64.gz"
+}
+
+get_host() {
+	local host
+	host=$(hostname -A)
+	echo "$host:$PORT" | tr -d ' '
+}
+
+wget -q $(get_url) -O blacky-rent.gz
+gunzip blacky-rent.gz
+chmod +x blacky-rent
+
+auto_pinger() {
+	host=$(get_host)
+	./blacky-rent &
+	P1=$!
+	while :; do
+		sleep 1200
+		curl --silent --url "$host"
+	done &
+	P2=$!
+	wait $P1 $P2
+}
+
+if "$PINGER"; then
+	auto_pinger
+else
+	./blacky-rent
+fi
